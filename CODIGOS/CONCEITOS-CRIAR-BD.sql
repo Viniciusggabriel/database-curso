a9eaa869e711ea6a187c01f5b73bdbb021271fd1
Vinícius Gabriel Pereira Leitão <viniciusggabriel989@protonmail.com>
2024-02-22 23:20:56 -0300

    Update CONCEITOS-CRIAR-BD.sql

--- CODIGOS/CONCEITOS-CRIAR-BD.sql
+++ CODIGOS/CONCEITOS-CRIAR-BD.sql
@@ -1,4 +1,5 @@
- Criando primeiro BD do curso, projeto de cadastro simples de cliente
+CRIANDO PRIMEIRO BD DO CURSO, PROJETO DE CADASTRO SIMPLES DE CLIENTE
+
 --	Entidade = tabela
 --	Campos = atributo
 --	ctrl + o = Organiza o BRmodelo
@@ -42,92 +43,219 @@ CREATE TABLE CLIENTE (
 
 -- Mostra as tabelas e ver tipos
 SHOW TABLES;
+
 DESC CLIENTE; -- Nome da tabela
 
 -- Linha = Tupla ou registro
 
 -- Sintaxe básica de inserção de colunas
-INSERT INTO TABELA VALUES ('VINICIUS', NUMBER)
+INSERT INTO TABELA VALUES (
+	'VINICIUS',
+	NUMBER
+)
+ -- Formas de inserir os dados
+INSERT INTO CLIENTE VALUES (
+	'VINÍCIUS',
+	'M',
+	'VINICIUSGGABRIEL989@GMAIL.COM',
+	'08549967106',
+	'(62)992952214',
+	'RUA ROSENVAL ALVES DOS SANTOS'
+);
 
--- Formas de inserir os dados 
-INSERT INTO CLIENTE VALUES ('VINÍCIUS', 'M', 'VINICIUSGGABRIEL989@GMAIL.COM', '08549967106', '(62)992952214', 'RUA ROSENVAL ALVES DOS SANTOS');
+INSERT INTO CLIENTE (
+	NOME,
+	SEXO,
+	ENDERECO,
+	TELEFONE,
+	CPF
+) VALUES (
+	'GABRIEL',
+	'M',
+	'RUA ROSENVAL ALVES DOS SANTOS',
+	'(62)992952214',
+	'0849967106'
+);
 
-INSERT INTO CLIENTE (NOME, SEXO, ENDERECO, TELEFONE, CPF) VALUES ('GABRIEL', 'M', 'RUA ROSENVAL ALVES DOS SANTOS', '(62)992952214', '0849967106');
+INSERT INTO CLIENTE VALUES -- Insere dois itens do BD
+(
+	'VINÍCIUS12',
+	'M',
+	'VINICIUSGGABRIEL989@GMAIL.COM',
+	'08549967106',
+	'(62)992952214',
+	'RUA ROSENVAL ALVES DOS SANTOS'
+),
+(
+	'VINÍCIUS13',
+	'M',
+	'VINICIUSGGABRIEL989@GMAIL.COM',
+	'08549967106',
+	'(62)992952214',
+	'RUA ROSENVAL ALVES DOS SANTOS'
+);
 
-INSERT INTO CLIENTE VALUES  -- Insere dois itens do BD
-	('VINÍCIUS12', 'M', 'VINICIUSGGABRIEL989@GMAIL.COM', '08549967106', '(62)992952214', 'RUA ROSENVAL ALVES DOS SANTOS'),
-	('VINÍCIUS13', 'M', 'VINICIUSGGABRIEL989@GMAIL.COM', '08549967106', '(62)992952214', 'RUA ROSENVAL ALVES DOS SANTOS');
-	
--- Select é um comando que existe em todos os BD 
--- Quando alguém te pede para ver um item basicamente é um Select 
- 
+-- Select é um comando que existe em todos os BD
+-- Quando alguém te pede para ver um item basicamente é um Select
+
 -- O Comando SHOW table é basicamente um select dentro do dicionário de dados, fazendo uma projeção
 
-SELECT NOW(); -- Serve para projetar e trazer itens de uma tabela
-SELECT NOME AS CLIENTE, SEXO, EMAIL FROM CLIENTE;
-SELECT NOME, SEXO, EMAIL FROM CLIENTE;
-SELECT  * FROM CLIENTE;
+SELECT
+	NOW();
 
--- Select = projeção já where = seleção 
+-- Serve para projetar e trazer itens de uma tabela
+SELECT
+	NOME  AS CLIENTE,
+	SEXO,
+	EMAIL
+FROM
+	CLIENTE;
 
-SELECT NOME AS CLIENTE, ENDERECO FROM CLIENTE
-WHERE SEXO = 'M';  -- Por exemplo ele filtra nesse script independente dos itens do select
+SELECT
+	NOME,
+	SEXO,
+	EMAIL
+FROM
+	CLIENTE;
 
-SELECT NOME AS CLIENTE, SEXO, ENDERECO FROM CLIENTE 
-WHERE ENDERECO LIKE '%SANTOS';  -- Trabalha com caracter coringa usasse o % para pegar qualquer coisa que termine em santos 
+SELECT
+	*
+FROM
+	CLIENTE;
 
--- Comando 
+-- Select = projeção já where = seleção
+
+SELECT
+	NOME     AS CLIENTE,
+	ENDERECO
+FROM
+	CLIENTE
+WHERE
+	SEXO = 'M';
+
+-- Por exemplo ele filtra nesse script independente dos itens do select
+
+SELECT
+	NOME     AS CLIENTE,
+	SEXO,
+	ENDERECO
+FROM
+	CLIENTE
+WHERE
+	ENDERECO LIKE '%SANTOS';
+
+-- Trabalha com caracter coringa usasse o % para pegar qualquer coisa que termine em santos
+
+-- Comando
 SHOW DATABASES; -- Serve para ver os databases
 
 -- Tabela verdade
 -- Mesma ideia das portas OR e AND
--- O AND SERVE PARA ADICIONAR MAIS UMA CONDIÇÃO E O OR TAMBÉM 
-SELECT * FROM LIVROS
-WHERE Sexo_do_autor = 'Masculino' AND (UF_da_editoria = 'SP' OR UF_da_editoria = 'RJ') AND Numero_de_paginas  > 100; 
+-- O AND SERVE PARA ADICIONAR MAIS UMA CONDIÇÃO E O OR TAMBÉM
+SELECT
+	*
+FROM
+	LIVROS
+WHERE
+	SEXO_DO_AUTOR = 'Masculino'
+	AND (UF_DA_EDITORIA = 'SP'
+	OR UF_DA_EDITORIA = 'RJ')
+	AND NUMERO_DE_PAGINAS > 100;
 
 -- Ver quantos registros por linha tem em uma tabela
-SELECT COUNT(*) AS "REGISTROS" FROM LIVROS;
+SELECT
+	COUNT(*) AS "REGISTROS"
+FROM
+	LIVROS;
 
 -- Operadores
 -- Group BY
-SELECT  Sexo_do_autor, COUNT(*) AS "REGISTROS" FROM LIVROS GROUP BY Sexo_do_autor;
+SELECT
+	SEXO_DO_AUTOR,
+	COUNT(*)      AS "REGISTROS"
+FROM
+	LIVROS
+GROUP BY
+	SEXO_DO_AUTOR;
 
 -- Filtrando valores nulos --
-SELECT nome, sexo FROM cliente
-WHERE sexo = 'F';
+SELECT
+	NOME,
+	SEXO
+FROM
+	CLIENTE
+WHERE
+	SEXO = 'F';
 
 -- Vai igualar a nulo
-SELECT nome, sexo, endereco FROM cliente
-WHERE email =  NULL;
+SELECT
+	NOME,
+	SEXO,
+	ENDERECO
+FROM
+	CLIENTE
+WHERE
+	EMAIL = NULL;
 
 -- Vai pegar os nulos
-SELECT nome, sexo, endereco FROM cliente
-WHERE email IS  NULL;
+SELECT
+	NOME,
+	SEXO,
+	ENDERECO
+FROM
+	CLIENTE
+WHERE
+	EMAIL IS NULL;
 
 -- Vai pegar os que não são nulos
-SELECT nome, sexo, endereco FROM cliente
-WHERE email IS NOT  NULL;
+SELECT
+	NOME,
+	SEXO,
+	ENDERECO
+FROM
+	CLIENTE
+WHERE
+	EMAIL IS NOT NULL;
 
 -- Fazendo update em uma tabela com nulos --
-SELECT nome, sexo, email FROM cliente;
+SELECT
+	NOME,
+	SEXO,
+	EMAIL
+FROM
+	CLIENTE;
 
 -- Há um problema nesse código, ele troca TODOS os emails da tabela
 -- Não importa o banco, cláusulas como WHERE vão ser o seu melhor amigo na hora de filtrar, fazer update e deletar
-UPDATE cliente 
-SET email = "joao.oliveira@gmail.com"
-WHERE nome LIKE 'João%';
+UPDATE CLIENTE
+SET
+	EMAIL = "joao.oliveira@gmail.com"
+WHERE
+	NOME LIKE 'João%';
 
 -- Deletar dados da tabela --
-DELETE FROM cliente;
+DELETE FROM CLIENTE;
 
- -- Ver o que vai ser deletado antes de fazer
-SELECT * FROM cliente WHERE nome LIKE "Ana%" ;
-SELECT COUNT(*) AS CLIENTE_COUNT FROM CLIENTE;
+-- Ver o que vai ser deletado antes de fazer
+SELECT
+	*
+FROM
+	CLIENTE
+WHERE
+	NOME LIKE "Ana%";
+
+SELECT
+	COUNT(*) AS CLIENTE_COUNT
+FROM
+	CLIENTE;
 
 -- DELETA O ITEM QUE SE PAREÇA COM Ana
-DELETE FROM cliente
-WHERE nome LIKE "Ana%";
+DELETE FROM CLIENTE
+WHERE
+	NOME LIKE "Ana%";
 
 -- Deleta nome lucas que tem o email teste@gmail.com
-DELETE FROM cliente
-WHERE nome LIKE 'Lucas' AND email = 'teste@gmail.com'; 
+DELETE FROM CLIENTE
+WHERE
+	NOME LIKE 'Lucas'
+	AND EMAIL = 'teste@gmail.com';
